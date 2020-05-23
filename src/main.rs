8b56d0e55a82dfefc3d34f24700e6e55dbb9b89a
simon-wf <simonwoodburyforget@gmail.com>
2020-05-23 04:16:54 -0400

    implemented colors conversion and next-day

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,87 @@
-use std::{
-    fmt::{self, Display, Formatter},
-    thread::sleep,
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
+use std::{thread::sleep, time::Duration};
+use time::{OffsetDateTime, PrimitiveDateTime, Time, UtcOffset};
 
-const GRAY: u32 = 0xaa_aa_aa;
-// const RED: u32 = 0xff_00_00;
-// const YELLOW: u32 = 0xff_00_ff;
-// const BLUE: u32 = 0x00_00_ff;
-const GREEN: u32 = 0x00_ff_00;
-// const CYAN: u32 = 0x00_ff_ff;
+mod display_impl {
+    use super::{Color, Fc, Hex2};
+    use std::fmt::{self, Display, Formatter};
 
-struct Colored(u32, u8);
+    impl From<Color> for u32 {
+        fn from(other: Color) -> u32 {
+            use Color::*;
+            match other {
+                Gray => 0x99_99_99,
+                LightGray => 0xcc_cc_cc,
+                Red => 0xff_00_00,
+                Yellow => 0xff_00_ff,
+                Blue => 0x00_00_ff,
+                Green => 0x00_ff_00,
+                Cyan => 0x00_ff_ff,
+            }
+        }
+    }
 
-impl Display for Colored {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let Self(a, b) = self;
-        write!(f, "<fc=#{:06x}>{:02x}</fc>", a, b)
+    impl Display for Color {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let x: u32 = (*self).into();
+            write!(f, "{:06x}", x)
+        }
+    }
+
+    impl<A: Display, B: Display> Display for Fc<A, B> {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let Self(a, b) = self;
+            write!(f, "<fc=#{}>{}</fc>", a, b)
+        }
+    }
+
+    impl<T: Display + fmt::UpperHex> Display for Hex2<T> {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f, "{:02X}", self.0)
+        }
     }
 }
 
-fn print_hextime(x: u64) {
-    let [.., d, c, b, a] = x.to_be_bytes();
-    println!(
-        "{} {} {} {}",
-        Colored(GRAY, d),
-        Colored(GRAY, c),
-        Colored(GREEN, b),
-        Colored(GRAY, a),
-    );
-    sleep(Duration::from_secs(1));
+#[derive(Clone, Copy)]
+enum Color {
+    Gray,
+    LightGray,
+    Red,
+    Yellow,
+    Blue,
+    Green,
+    Cyan,
+}
+
+struct Fc<A, B>(A, B);
+struct Hex2<T>(T);
+
+fn print_full(x: u32) {
+    match x.to_be_bytes() {
+        [d, c, b, a] => print!(
+            "{} {} {} {}",
+            Fc(Color::Gray, Hex2(d)),
+            Fc(Color::Gray, Hex2(c)),
+            Fc(Color::Green, Hex2(b)),
+            Fc(Color::LightGray, Hex2(a)),
+        ),
+    }
+}
+
+fn print_two(x: u32) {
+    let [.., b, a] = x.to_be_bytes();
+    print!("{} {}", Fc(Color::Cyan, b), Fc(Color::LightGray, a),);
 }
 
 fn main() {
-    std::iter::repeat_with(SystemTime::now)
-        .map(|t| t.duration_since(UNIX_EPOCH))
-        .for_each(|r| match r {
-            Ok(x) => print_hextime(x.as_secs()),
-            Err(_) => println!("..time-travelling"),
-        })
+    std::iter::repeat_with(OffsetDateTime::now_local).for_each(|now| {
+        print_full(now.timestamp() as u32);
+        print!(" - ");
+        print_two(
+            PrimitiveDateTime::new(now.date().next_day(), Time::midnight())
+                .assume_offset(UtcOffset::current_local_offset())
+                .timestamp() as u32,
+        );
+        println!();
+        sleep(Duration::from_secs(1));
+    });
 }
