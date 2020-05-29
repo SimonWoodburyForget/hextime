b6d6ee6765790423cfede2284caefa600c455063
simon-wf <simonwoodburyforget@gmail.com>
2020-05-29 19:28:58 -0400

    replaced formatting with HexTime; reordered module

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,42 @@ use std::{
 };
 use structopt::StructOpt;
 
+mod cli {
+    use structopt::StructOpt;
+
+    #[derive(Debug, Clone, Copy)]
+    pub enum Mode {
+        Xmobar,
+        Terminal,
+        ColorTerm,
+    }
+
+    impl Mode {
+        const STRS: [&'static str; 3] = ["xmobar", "terminal", "cterm"];
+        const MODE: [Mode; 3] = [Mode::Xmobar, Mode::Terminal, Mode::ColorTerm];
+    }
+
+    impl std::str::FromStr for Mode {
+        type Err = &'static str;
+        fn from_str(input: &str) -> Result<Self, Self::Err> {
+            Mode::STRS
+                .iter()
+                .zip(Mode::MODE.iter())
+                .find_map(|(&x, &m)| if x == input { Some(m) } else { None })
+                .ok_or("no match")
+        }
+    }
+
+    #[derive(StructOpt, Debug)]
+    pub struct Opt {
+        /// Printing mode.
+        #[structopt(short, long, possible_values = &Mode::STRS)]
+        pub mode: Mode,
+    }
+}
+
 mod formatting {
+    use super::HexTime;
     use std::fmt::{self, Display, Formatter};
 
     #[derive(Clone, Copy)]
@@ -62,22 +97,58 @@ mod formatting {
     pub struct XmobarFmt<T>(pub T);
     impl Display for XmobarFmt<u32> {
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-            use Color::*;
-            self.0
-                .to_be_bytes()
-                .iter()
-                .zip([Gray, Gray, Green, LightGray].iter())
-                .try_for_each(|(&x, &c)| write!(f, "{} ", Fc(c, Hex2(x))))
+            HexTime(self.0)
+                .segmented()
+                .map(|(s, x)| Fc(Color::from(s), Hex2(x)))
+                .try_for_each(|x| write!(f, "{} ", x))
         }
     }
 
     pub struct TermFmt<T>(pub T);
     impl Display for TermFmt<u32> {
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-            self.0
-                .to_be_bytes()
-                .iter()
-                .try_for_each(|&x| write!(f, "{} ", Hex2(x)))
+            HexTime(self.0)
+                .bytes()
+                .try_for_each(|x| write!(f, "{} ", Hex2(x)))
+        }
+    }
+}
+
+mod seg {
+    /// Segment of HexTime.
+    #[derive(Clone, Copy)]
+    pub struct Segment(usize);
+
+    impl Segment {
+        /// Return offset segment of HexTime. *Value must be smaller then 8.*
+        pub fn new(x: usize) -> Self {
+            if x < 8 {
+                Self(x)
+            } else {
+                panic!("Time segment is too large.")
+            }
+        }
+    }
+
+    impl From<Segment> for crossterm::style::Color {
+        fn from(seg: Segment) -> Self {
+            use crossterm::style::Color::*;
+            match seg.0 {
+                0 => White,
+                1 => Green,
+                _ => DarkGrey,
+            }
+        }
+    }
+
+    impl From<Segment> for crate::formatting::Color {
+        fn from(seg: Segment) -> Self {
+            use crate::formatting::Color::*;
+            match seg.0 {
+                0 => LightGray,
+                1 => Green,
+                _ => Gray,
+            }
         }
     }
 }
@@ -93,36 +164,10 @@ impl HexTime {
     }
 
     /// Iterator over increasing bytes of time value, with an offset from zero segment.
-    fn segmented(self) -> impl Iterator<Item = (Segment, u8)> {
+    fn segmented(self) -> impl Iterator<Item = (seg::Segment, u8)> {
         self.bytes()
             .enumerate()
-            .map(move |(a, b)| (Segment::new(self.0.to_be_bytes().len() - 1 - a), b))
-    }
-}
-
-/// Segment of HexTime.
-#[derive(Clone, Copy)]
-struct Segment(usize);
-
-impl Segment {
-    /// Return offset segment of HexTime. *Value must be smaller then 8.*
-    fn new(x: usize) -> Self {
-        if x < 8 {
-            Self(x)
-        } else {
-            panic!("Time segment is too large.")
-        }
-    }
-}
-
-impl From<Segment> for crossterm::style::Color {
-    fn from(seg: Segment) -> Self {
-        use crossterm::style::Color::*;
-        match seg.0 {
-            0 => White,
-            1 => Green,
-            _ => DarkGrey,
-        }
+            .map(move |(a, b)| (seg::Segment::new(self.0.to_be_bytes().len() - 1 - a), b))
     }
 }
 
@@ -161,40 +206,6 @@ fn seconds() -> impl Iterator<Item = u32> {
         .map(|time| time.duration_since(UNIX_EPOCH))
         .filter_map(|result| result.map(|x| x.as_secs()).ok())
         .map(|secs| secs.try_into().expect("Time overflowed."))
-}
-
-mod cli {
-    use structopt::StructOpt;
-
-    #[derive(Debug, Clone, Copy)]
-    pub enum Mode {
-        Xmobar,
-        Terminal,
-        ColorTerm,
-    }
-
-    impl Mode {
-        const STRS: [&'static str; 3] = ["xmobar", "terminal", "cterm"];
-        const MODE: [Mode; 3] = [Mode::Xmobar, Mode::Terminal, Mode::ColorTerm];
-    }
-
-    impl std::str::FromStr for Mode {
-        type Err = &'static str;
-        fn from_str(input: &str) -> Result<Self, Self::Err> {
-            Mode::STRS
-                .iter()
-                .zip(Mode::MODE.iter())
-                .find_map(|(&x, &m)| if x == input { Some(m) } else { None })
-                .ok_or("no match")
-        }
-    }
-
-    #[derive(StructOpt, Debug)]
-    pub struct Opt {
-        /// Printing mode.
-        #[structopt(short, long, possible_values = &Mode::STRS)]
-        pub mode: Mode,
-    }
 }
 
 fn main() {
